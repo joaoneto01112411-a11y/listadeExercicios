a45e74f4b58878e316bbe3120daec7e03657c618
joaoneto01112411-a11y <joao.neto01112411@sesisenaipr.org.br>
2026-03-02 12:41:07 +0000

    ListadeExercicios

--- lista14.js
+++ lista14.js
@@ -1,4 +1,4 @@
 //Criar um programa que mostre um desconto de 5% para compras maiores que R$100
 let compra = 120;
-let desconto = * 0.95;
+let desconto = compra *0.95;
 console.log(`o desconto de 5% da compra de 120 é ${desconto}`)
